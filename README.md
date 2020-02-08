525c2a06ba42e10d92f6bff75113223e49cc05cd
Dave Schumaker <dave@dave.ly>
2020-02-08 08:51:39 -0800

    Add support for subdirectories in Webpack

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Starter
+
+There are many React starter projects around the Internet, but this one is mine.
+
+I'm using this project to quickly setup my own React projects without the overhead of Create React App. Plus, it's nice to know how to setup the dev and production build process using tools like Webpack and Babel.
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,28 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PORT = 8080;
 
 module.exports = {
   entry: './src/app/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: {
+      index: 'index.html',
+      rewrites: [{ from: /./, to: '/index.html' }]
+    },
+    port: PORT
+  },
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   resolve: {
     alias: {
       root: path.resolve(__dirname),
+      app: path.resolve(__dirname, 'src/app/'),
       src: path.resolve(__dirname, 'src/')
     }
   },
